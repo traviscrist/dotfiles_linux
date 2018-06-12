748f852d4b7541aa2e230256807201b9441b501f
Travis Crist <4206970+traviscrist@users.noreply.github.com>
2018-06-11 21:30:06 -0700

    more gnome-i3 setup

--- scripts/i3-set-config.sh
+++ scripts/i3-set-config.sh
@@ -10,13 +10,13 @@ killall -q polybar
 
 case $1 in
   roadwarrior)
-    xrandr --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP2 --off --output DP1 --off
+    xrandr --output eDP1 --dpi 130 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP2 --off --output DP1 --off
     xrandr --output HDMI1 --mode 1920x1080 --same-as eDP1
     I3_CONFIG=roadwarrior
     ;;
   office)
     #xrandr --output DP2 --primary --mode 3440x1440 --pos 2560x0 --rotate normal --output eDP1 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI1 --off --output DP2 --off
-    xrandr --output DP1 --primary --mode 3840x1600 --rotate normal --above eDP1 --output eDP1 --mode 2560x1440 --rotate normal --output HDMI1 --off --output DP2 --off
+    xrandr --output DP1 --primary --mode 3840x1600 --rotate normal --above eDP1 --output eDP1 --dpi 130 --mode 2560x1440 --rotate normal --output HDMI1 --off --output DP2 --off
     I3_CONFIG=office
     ;;
   extend)
@@ -38,7 +38,7 @@ cat \
   > ~/.config/i3/config
 
 # Re-enable caps:escape
-#setxkbmap -option caps:escape
+setxkbmap -option caps:escape
 
 # Restart i3
 i3-msg restart
