2da5d7fa72a9dc31542dc27e3230250c793ef44b
Travis Crist <4206970+traviscrist@users.noreply.github.com>
2018-06-22 09:27:42 -0700

    move to emacs to fix ctrl a not working in xfce4 terminal

--- scripts/i3-set-config.sh
+++ scripts/i3-set-config.sh
@@ -8,8 +8,6 @@ chmod +x /home/travis/bin/*
 # Polybar doesn't like this at all
 killall -q polybar
 
-# ADD DPI Back here? 
-
 case $1 in
   roadwarrior)
     xrandr --output eDP1 --dpi 130 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP2 --off --output DP1 --off
