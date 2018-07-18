d2b9023a7de0bcbc7377e42e0b246dfd07f7a7e1
Travis Crist <4206970+traviscrist@users.noreply.github.com>
2018-07-17 17:07:11 -0700

    fix scaling problems on laptop

--- scripts/i3-set-config.sh
+++ scripts/i3-set-config.sh
@@ -10,8 +10,8 @@ killall -q polybar
 
 case $1 in
   roadwarrior)
-    xrandr --output eDP1 --dpi 130 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP2 --off --output DP1 --off
-    xrandr --output HDMI1 --mode 1920x1080 --same-as eDP1
+    xrandr --output eDP1 --dpi 130 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP2 --off --output DP1 --off --output HDMI1 --off
+    #xrandr --output HDMI1 --mode 1920x1080 --same-as eDP1
     I3_CONFIG=roadwarrior
     ;;
   office)
